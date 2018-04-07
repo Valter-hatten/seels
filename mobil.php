376ef9f344847cfae0ae00268ef862d652c457a4
Valter-hatten <35872074+Valter-hatten@users.noreply.github.com>
2018-04-07 22:50:34 +0200

    Site Clone

---
+++ mobil.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Media Player HTML</title>
+	<link rel="stylesheet" href="css/base.css">
+</head>
+<body>
+
+<div class="media">
+	
+	<video class="hidden-xs hidden-sm hidden-md hidden-custom videosize embed-responsive-item" autoplay="autoplay" loop="loop">
+  		<source src="https://videos2.sendvid.com/c6/02/rbdn35sx.mp4" type='video/mp4;codecs="avc1.42E01E, mp4a.40.2"'>
+			Your browser doesn't support HTML5 video playback.
+			You can <a href="media/video.mp4">Download the video instead</a>.
+		</p>
+	</video>
+
+</div>
+
+	
+</div>
+
+</body>
+</html>
